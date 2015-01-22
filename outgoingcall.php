4e5c0a7d809b0cf7b78cdf3478947783ac01cc4c
wgins <willginsberg@gmail.com>
2015-01-22 17:14:48 -0600

    Update outgoingcall.php

--- outgoingcall.php
+++ outgoingcall.php
@@ -1,4 +1,5 @@
 <?php
+//SENSITIVE DATABASE / TABLE INFORMATION, PHONE NUMBERS, PINS, TWILIO CREDENTIALS HAVE BEEN REMOVED
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 echo "<Response>
 		<Say>You entered " . $_REQUEST['Digits'] . "</Say>
