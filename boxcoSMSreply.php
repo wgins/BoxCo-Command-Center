6fa4ebb8de6e0a37cd34b9f777c2185930f8784a
wgins <willginsberg@gmail.com>
2015-01-22 16:33:41 -0600

    Create boxcoSMSreply.php

---
+++ boxcoSMSreply.php
@@ -0,0 +1,10 @@
+<?php
+/*A simple reply doc that is used for replies to all incoming texts to BoxCo. command center. Edit the message as needed.
+**WILL GINSBERG**
+**June, 2014** */
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Message>Thank you for contacting BoxCo. Please email us at xxxxx@nuboxco.com with any questions or concerns. Thanks!</Message>
+</Response>
