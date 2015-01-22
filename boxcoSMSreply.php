a8a12da8e97e6db2f4d793bd3529e341ada77ee6
wgins <willginsberg@gmail.com>
2015-01-22 17:14:30 -0600

    Update boxcoSMSreply.php

--- boxcoSMSreply.php
+++ boxcoSMSreply.php
@@ -1,7 +1,10 @@
 <?php
 /*A simple reply doc that is used for replies to all incoming texts to BoxCo. command center. Edit the message as needed.
 **WILL GINSBERG**
-**June, 2014** */
+**June, 2014** 
+SENSITIVE DATABASE / TABLE INFORMATION, PHONE NUMBERS, PINS, TWILIO CREDENTIALS HAVE BEEN REMOVED
+*/
+
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
