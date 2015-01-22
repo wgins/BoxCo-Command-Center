03e3fdcaab83fe05fd6a48896bb8d11f911ba001
wgins <willginsberg@gmail.com>
2015-01-22 17:12:25 -0600

    Update helloBoxco.php

--- helloBoxco.php
+++ helloBoxco.php
@@ -8,7 +8,7 @@ header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-<Gather numDigits="6" action="boxcoinputhandler.php" method="POST">
+<Gather numDigits="6" action="boxcoInputHandler.php" method="POST">
 	<Say voice="woman" language="en-gb">Thank you for calling BoxCo. To speak with our manager on duty, please press 1. To hear more info, please press 2. To disconnect, press 3.</Say> <Pause length="5"/>
 	<Pause length="2"/>
 	<Say voice="woman" language="en-gb">Thank you for calling BoxCo. To speak with our manager on duty, please press 1. To hear more info, please press 2. To disconnect, press 3.</Say> 
