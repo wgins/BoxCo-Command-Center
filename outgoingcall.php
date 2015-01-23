33a8920fbb4f90f19c455c80dd4d6107e192f594
Will Ginsberg <willginsberg@gmail.com>
2015-01-23 14:15:20 -0600

    Update outgoingcall.php

--- outgoingcall.php
+++ outgoingcall.php
@@ -1,5 +1,6 @@
 <?php
 //SENSITIVE DATABASE / TABLE INFORMATION, PHONE NUMBERS, PINS, TWILIO CREDENTIALS HAVE BEEN REMOVED
+//Make outgoing call from input handler php file. Once employee has entered pin, prompted to enter a number, this takes the number they enter and dials.
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 echo "<Response>
 		<Say>You entered " . $_REQUEST['Digits'] . "</Say>
