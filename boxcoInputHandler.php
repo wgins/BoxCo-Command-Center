60eb9d92f888e3eaca2db49dbb2860a056341e2f
Will Ginsberg <willginsberg@gmail.com>
2015-01-23 14:09:36 -0600

    Update boxcoInputHandler.php

--- boxcoInputHandler.php
+++ boxcoInputHandler.php
@@ -15,7 +15,9 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 <Response>
 <?php
 $time = date('Hi');
-      function getEmployee ()
+
+//Connects to database to determine which employee has been set in the database as 'manager on duty'.
+function getEmployee ()
       {
           $time = date('Hi');
           $link = mysqli_connect('xxxxxx', 'xxxxxxx', 'xxxxxx', 'xxxxxxx');
@@ -26,7 +28,8 @@ $time = date('Hi');
             return $currentNum;  
       }
          $curEmp = getEmployee();
-    
+
+//If customer presses one, customer is directed to manager on duty.
 if ($_REQUEST['Digits'] == '1')
   {
     ?>
@@ -37,6 +40,8 @@ if ($_REQUEST['Digits'] == '1')
 
 <?php
   }
+  
+  //If customer presses two, instructs them to visit the website.
 else if ($_REQUEST['Digits'] == '2')
   {
     ?>
@@ -45,6 +50,7 @@ else if ($_REQUEST['Digits'] == '2')
            
 <?php
   }
+  //If customer presses three, also instructs customer to vsiti website.
 else if ($_REQUEST['Digits'] == '3')
   { 
     ?>
@@ -52,6 +58,7 @@ else if ($_REQUEST['Digits'] == '3')
    <Hangup/>
     <?php
   }
+  //If employee dials in and enters pin, allows them to make outgoing call. Gathers the number the employee wants to call and then connects.
 else if ($_REQUEST['Digits'] == 'XXXXXXX')
   {
     ?>
