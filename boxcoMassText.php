f2710d77bd24fa39bb37d0550d38c0fd720cc802
Will Ginsberg <willginsberg@gmail.com>
2015-01-23 14:42:05 -0600

    Update boxcoMassText.php

--- boxcoMassText.php
+++ boxcoMassText.php
@@ -1,5 +1,5 @@
 <?php
-/* BoxCo. Mass Texting System. This allows users to send mass text to an array. It requires a pin to prevent any unintentional sending of text.
+/* BoxCo. Mass Texting System. This allows users to send mass text to an array. It requires a pin to prevent any malicious use.
     **WILL GINSBERG**
     **June, 2014**
     SENSITIVE DATABASE / TABLE INFORMATION, PHONE NUMBERS, PINS, TWILIO CREDENTIALS HAVE BEEN REMOVED
@@ -7,40 +7,40 @@
 
 $pin= $_POST['pin'];
 
+//if correct pin is entered, then send the text(s)
 if ($pin==xxxxxxx) {
 
 require "/boxco_dev/twilio-php-master/Services/Twilio.php";
- 
+//Twilio authentication information
     $AccountSid = "xxxxxxxx";
     $AuthToken = "xxxxxxx";
     $client = new Services_Twilio($AccountSid, $AuthToken);
  
 
-    // Feel free to change/add your own phone number and name here.
+//Add all numbers to this array that you desire to send your text to.
     $people = array(
          "xxxxxxxxxxxxxxx",
     );
  
- 
- // $name is the name next to it
-    foreach ($people as $number) {
+   foreach ($people as $number) {
  
         $sms = $client->account->messages->sendMessage(
  
         //THIS IS THE FROM NUMBER, DO NOT CHANGE THIS! PLEASE!
             "xxxxxxxxxx", 
  
-            // the number we are sending to - Any phone number
+            //Takes number from array
             $number,
  
-            // the sms body
-            "Hi this is BoxCo. You are scheduled to work tomorrow (6/2), please refer to the google spreadsheet to confirm your time. If you have any questions or concerns, email us at xxxxxx@nuboxco.com. Thanks!"
+            //TEXT BODY
+            "Hi this is BoxCo. You are scheduled to work tomorrow (1/10), please refer to the google spreadsheet to confirm your time. If you have any questions or concerns, email us at xxxxxx@nuboxco.com. Thanks!"
         );
  
         // Display a confirmation message on the screen
         echo "Sent message to $number . Thanks! </br>";
                                 }
         }
+//Otherwise say that you didn't enter the right pin.
 else {
     print "You either failed to enter a pin or entered the wrong pin. Please try again.";
 }
